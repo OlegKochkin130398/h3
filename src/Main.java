99b43b161fe8813089ac29f6ba091be21cfaa9fa
oleg- <o.boys@yandex,ru>
2022-08-19 16:28:17 +0300

    3 Задание.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int year = 2021;
+        if(year % 4 == 0 && year % 100 !=0 || year % 400 == 0){
+            System.out.println("Год высокосный!");
+        }
+        else System.out.println("Год невысокосный");
+    }
 }
